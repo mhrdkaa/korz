8e8f327064a87f957eca4139041081eeaac7a7d5
Putra Mahardika <148011331+mhrdkaa@users.noreply.github.com>
2026-04-06 14:50:56 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
-  if (url.pathname === '/int') {
+
+  if (url.pathname.endsWith('/int')) {
     const targetUrl = url.searchParams.get('url');
+
     if (targetUrl) {
       event.respondWith(
         fetch(targetUrl, {
           mode: 'cors', 
-        })
+        }).catch(err => new Response("Fetch Error: " + err))
       );
     }
   }
