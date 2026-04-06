73c51bfb6504c30affc757d12c38c480746e5daa
Putra Mahardika <148011331+mhrdkaa@users.noreply.github.com>
2026-04-06 14:41:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
-  if (url.pathname === '/int') {
+  if (url.pathname === '?') {
     const targetUrl = url.searchParams.get('url');
     if (targetUrl) {
       event.respondWith(
