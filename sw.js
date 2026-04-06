ea7885e1f7006e3f83870e00aa08de38cd237912
Putra Mahardika <148011331+mhrdkaa@users.noreply.github.com>
2026-04-06 14:37:56 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-// kont
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname === '/int') {
+    const targetUrl = url.searchParams.get('url');
+    if (targetUrl) {
+      event.respondWith(
+        fetch(targetUrl, {
+          mode: 'cors', 
+        })
+      );
+    }
+  }
+});
